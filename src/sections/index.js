807a5b3785e7e170c3af5778f23b25077917a7b6
Jean-Marc18 <koffijm18@gmail.com>
2023-08-19 19:49:02 +0000

    first commit

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from './Hero';
+import PopularProducts from './PopularProducts';
+import SuperQuality from './SuperQuality';
+import Footer from './Footer';
+import Services from './Services';
+import SpecialOffer from './SpecialOffer';
+import CustomerReviews from './CustomerReviews';
+import Subscribe from './Subscribe';
+
+export {
+    Hero,
+    PopularProducts,
+    Services,
+    SpecialOffer,
+    SuperQuality,
+    CustomerReviews,
+    Subscribe,
+    Footer
+}
